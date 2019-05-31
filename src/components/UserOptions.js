9d5cf45b3f76710dc6c459a9851eb1ea35edcf72
Unnit Metaliya <unnitjmetaliya@gmail.com>
2019-05-31 18:51:53 -0400

    finally accomplished responsive grid layout

--- src/components/UserOptions.js
+++ src/components/UserOptions.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Select, Card } from "antd";
+import { Select, Card, List } from "antd";
 
 class UserOptions extends Component {
   constructor(props) {
@@ -51,24 +51,45 @@ class UserOptions extends Component {
   render() {
     let { Option } = Select;
     return (
-      <div className="User-selection">
-        <Select
-          defaultValue="Select User"
-          style={{ width: 220 }}
-          onChange={this.handleChange}
-        >
-          {this.state.users.map(user => (
-            <Option key={user.id}>{user.name}</Option>
-          ))}
-        </Select>
-        <div className="User-albums">
-          <Card>
-            {this.state.userAlbums.map(album => (
-              <Card.Grid key={album.id} onClick={this.handleAlbumClick}>
-                {album.title}
-              </Card.Grid>
+      <div className="container">
+        <div className="User-selection">
+          <Select
+            defaultValue="Select User"
+            style={{ width: 220 }}
+            onChange={this.handleChange}
+          >
+            {this.state.users.map(user => (
+              <Option key={user.id}>{user.name}</Option>
             ))}
-          </Card>
+          </Select>
+          {/* <div className="User-albums">
+            <Card>
+              {this.state.userAlbums.map(album => (
+                <Card.Grid key={album.id} onClick={this.handleAlbumClick}>
+                  {album.title}
+                </Card.Grid>
+              ))}
+            </Card>
+          </div> */}
+        </div>
+        <div className="User-albums-list">
+          <List
+            grid={{
+              gutter: 16,
+              xs: 1,
+              sm: 2,
+              md: 4,
+              lg: 4,
+              xl: 2,
+              xxl: 3
+            }}
+            dataSource={this.state.userAlbums}
+            renderItem={item => (
+              <List.Item>
+                <Card title={`Album # - ` + item.id}>{item.title}</Card>
+              </List.Item>
+            )}
+          />
         </div>
       </div>
     );
